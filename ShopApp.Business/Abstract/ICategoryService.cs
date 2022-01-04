1a2871ba0a8baed925e006e716cb58a70f176d5e
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-04 13:11:12 +0300

    Upd

---
+++ ShopApp.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApp.Entities;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+    }
+}
