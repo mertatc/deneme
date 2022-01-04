99eb181025414f6a5304e2143a9db727e3ac41eb
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-04 14:57:22 +0300

    UPD

---
+++ ShopApp.DataAccess/Concrete/Memory/MemoryProductDal.cs
@@ -0,0 +1,51 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.Memory
+{
+    public class MemoryProductDal : IProductDal //dan implement ediyoruz
+    {
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GatOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            var products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "SAMSUNG", ImageUrl = "1.jpg", Price = 1000 },
+                new Product() { Id = 1, Name = "SAMSUNG", ImageUrl = "1.jpg", Price = 1000 },
+                new Product() { Id = 1, Name = "SAMSUNG", ImageUrl = "1.jpg", Price = 1000 },
+            };
+
+            return products;
+        }
+
+        public Product GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShopApp.Business.Abstract;
+using ShopApp.Business.Concrete;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +26,9 @@ namespace ShopApp.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddScoped<IProductDal, MemoryProductDal>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,16 +43,7 @@ namespace ShopApp.WebUI
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
