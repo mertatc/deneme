52be17971f3f71c50bb1c213c7c42c6b06bea4c5
Mehmet Mert Atıcı <mertatc1903@hotmail.com>
2022-01-06 15:44:59 +0300

    upd

--- ShopApp.DataAccess/Concrete/MySql/MySqlProductDal.cs
+++ ShopApp.DataAccess/Concrete/MySql/MySqlProductDal.cs
@@ -11,6 +11,34 @@ namespace ShopApp.DataAccess.Concrete.MySql
 {
     public class MySqlProductDal : IProductDal
     {
-        
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GatOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
